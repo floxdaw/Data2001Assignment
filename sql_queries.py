5ac271a484db25d4016994369682c1b093a9fb2b
D-Codespace <64941871+DavidCodespace@users.noreply.github.com>
2022-05-20 11:24:17 +1000

    updating the queries file

--- sql_queries.py
+++ sql_queries.py
@@ -3,18 +3,15 @@ from geoalchemy2 import Geometry
 
 db, conn = ds.pgconnect('Credentials.json')
 
+
+# number of halth services per 1000 people
 sql = """
-SELECT sa2_name, ST_Area(geom) AS total_area
-FROM sa2
+SELECT b.sa2_name, b.health_care_and_social_assistance/n.population AS ratio, s.geom
+FROM business_stats b, neighborhoods n, sa2 s
+WHERE b.sa2_name = n.sa2_name
+AND s.sa2_name = n.sa2_name
 LIMIT 5
 """
-
-sql = """
-SELECT sa2_name
-FROM business_stats
-LIMIT 5
-"""
-
 print(ds.query(conn, sql))
 
 
