87810aab543a3148c05b311b7053020331979d58
D-Codespace <64941871+DavidCodespace@users.noreply.github.com>
2022-05-20 11:55:45 +1000

    updating the queries file

--- sql_queries.py
+++ sql_queries.py
@@ -1,7 +1,19 @@
 import database_setup as ds
 from geoalchemy2 import Geometry
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 db, conn = ds.pgconnect('Credentials.json')
+
+#Burner query - just used for testing and debugging
+sql = """
+SELECT b.sa2_name, b.health_care_and_social_assistance AS count, (n.population/1000) AS pop
+FROM business_stats b, neighborhoods n, sa2 s
+WHERE b.sa2_name = n.sa2_name
+AND s.sa2_name = n.sa2_name
+ORDER by count DESC
+"""
+print(ds.query(conn, sql))
 
 
 # number of health services per 1000 people
@@ -10,10 +22,24 @@ SELECT b.sa2_name, b.health_care_and_social_assistance/(n.population/1000) AS ra
 FROM business_stats b, neighborhoods n, sa2 s
 WHERE b.sa2_name = n.sa2_name
 AND s.sa2_name = n.sa2_name
-LIMIT 5
+ORDER by ratio DESC
 """
 print(ds.query(conn, sql))
+health = gpd.read_postgis("""
+SELECT b.sa2_name, b.health_care_and_social_assistance/(n.population/1000) AS ratio,s.geom
+FROM business_stats b, neighborhoods n, sa2 s
+WHERE b.sa2_name = n.sa2_name
+AND s.sa2_name = n.sa2_name
+""", conn)
 
+health.plot(cmap='Set2', figsize=(10, 10))
+
+# number of schools catchment areas per 1000 ’young people
+sql = """
+SELECT c.use_id, c.catch_type, c.use_desc
+FROM catchments c
+"""
+print(ds.query(conn, sql))
 
 
 # ds.close_connection(conn, db)
