bdb228dad0b5323f391ac1380b3fc2a6a3995911
D-Codespace <64941871+DavidCodespace@users.noreply.github.com>
2022-05-20 19:15:34 +1000

    added and updated queries for Schools and Business

--- sql_queries.py
+++ sql_queries.py
@@ -1,30 +1,35 @@
 import database_setup as ds
+import pandas as pd
 from geoalchemy2 import Geometry
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
+# import geoplot #https://stackoverflow.com/questions/70177062/cartopy-not-able-to-identify-geos-for-proj-install-on-windows
+import mapclassify
+
 db, conn = ds.pgconnect('Credentials.json')
 
-#Burner query - just used for testing and debugging
+# Burner query - just used for testing and debugging
 sql = """
-SELECT b.sa2_name, b.health_care_and_social_assistance AS count, (n.population/1000) AS pop
-FROM business_stats b, neighborhoods n, sa2 s
-WHERE b.sa2_name = n.sa2_name
-AND s.sa2_name = n.sa2_name
-ORDER by count DESC
+SELECT s.sa2_name, COUNT(c.use_desc), n.sa2_code
+FROM sa2 s 
+JOIN catchments c ON (ST_CONTAINS(s.geom, c.geom) OR ST_OVERLAPS(s.geom, c.geom))
+FULL OUTER JOIN neighborhoods n USING (sa2_code)
+GROUP BY s.sa2_name
+ORDER BY count DESC
 """
 print(ds.query(conn, sql))
 
-
 # number of health services per 1000 people
 sql = """
-SELECT b.sa2_name, b.health_care_and_social_assistance/(n.population/1000) AS ratio,s.geom
+SELECT b.sa2_name, b.health_care_and_social_assistance/(n.population/1000) AS ratio
 FROM business_stats b, neighborhoods n, sa2 s
 WHERE b.sa2_name = n.sa2_name
 AND s.sa2_name = n.sa2_name
 ORDER by ratio DESC
 """
-print(ds.query(conn, sql))
+print(pd.DataFrame(ds.query(conn, sql)))
+
 health = gpd.read_postgis("""
 SELECT b.sa2_name, b.health_care_and_social_assistance/(n.population/1000) AS ratio,s.geom
 FROM business_stats b, neighborhoods n, sa2 s
@@ -32,14 +37,23 @@ WHERE b.sa2_name = n.sa2_name
 AND s.sa2_name = n.sa2_name
 """, conn)
 
-health.plot(cmap='Set2', figsize=(10, 10))
+# to correct for a business district with very few people living there, so that the later normalisation isn't whacked
+# out
+health.loc[health['ratio'] > 40, 'ratio'] = 40
 
-# number of schools catchment areas per 1000 ’young people
+health['ratio'] = health['ratio'].fillna(0)
+
+health['ratio_normalised'] = (health['ratio'] - health['ratio'].min()) / (health['ratio'].max() - health['ratio'].min())
+
+
+# number of school catchment areas per 1000 young people
 sql = """
-SELECT c.use_id, c.catch_type, c.use_desc
-FROM catchments c
+SELECT s.sa2_name, (CAST (COUNT (ca.use_desc) AS float)) AS s_count, (CAST (n.total_young_people AS float)/1000) AS young_people
+FROM sa2 s 
+JOIN catchments ca ON (ST_CONTAINS(s.geom, ca.geom) OR ST_OVERLAPS(s.geom, ca.geom))
+FULL OUTER JOIN neighborhoods n USING (sa2_code)
+GROUP BY s.sa2_name, young_people
 """
 print(ds.query(conn, sql))
-
 
 # ds.close_connection(conn, db)
